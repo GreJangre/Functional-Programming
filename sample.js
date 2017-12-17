d4e19eee301129acb7031030b989065f6c45d72c
Jangre <dlatjdwn8824@naver.com>
2017-12-17 16:32:40 +0900

    filter 함수를 이용한 리팩터링

--- sample.js
+++ sample.js
@@ -16,15 +16,22 @@ function f1() { return 100; }
 function f2() { return function() {}; }
 
 var users = [
-    { id: 1, name: 'ID', age: 36 },
-    { id: 2, name: 'BJ', age: 32 },
+    { id: 1, name: 'ID', age: 32 },
+    { id: 2, name: 'BJ', age: 25 },
     { id: 3, name: 'JM', age: 32 },
-    { id: 4, name: 'PJ', age: 27 },
-    { id: 5, name: 'HA', age: 25 },
-    { id: 6, name: 'JE', age: 26 },
-    { id: 7, name: 'JI', age: 31 },
-    { id: 8, name: 'MP', age: 23 }
+    { id: 4, name: 'PJ', age: 28 },
+    { id: 5, name: 'HA', age: 27 },
+    { id: 6, name: 'JE', age: 32 },
+    { id: 7, name: 'JI', age: 24 }
 ];
+
+function filter(list, predicate) {
+    var new_list = [];
+    for (var i = 0, len = list.length; i < len; i++) {
+        if (predicate(list[i])) { new_list.push(list[i]) }
+    }
+    return new_list;
+}
 
 var temp_users = [];
 for (var i = 0, len = users.length; i < len; i++) {
@@ -33,22 +40,31 @@ for (var i = 0, len = users.length; i < len; i++) {
 console.log(temp_users.length);
 // 4
 
+var users_under_30 = filter(users, function(user) { return user.age < 30 });
+console.log(users_under_30.length);
+// 4
+
 var ages = [];
-for (var i = 0, len = temp_users.length; i < len; i++) {
-    ages.push(temp_users[i].age);
+for (var i = 0, len = users_under_30.length; i < len; i++) {
+    ages.push(users_under_30[i].age);
 }
 console.log(ages);
-// [27, 25, 26, 23]
+// [25, 28, 27, 24]
 
 var temp_users = [];
 for (var i = 0, len = users.length; i < len; i++) {
     if (users[i].age >= 30) { temp_users.push(users[i]); }
 }
+console.log(temp_users.length);
+// 3
+
+var users_over_30 = filter(users, function(user) { return user.age >= 30 });
+console.log(users_over_30.length);
 // 3
 
 var names = [];
-for (var i = 0, len = temp_users.length; i < len; i++) {
-    names.push(temp_users[i].name);
+for (var i = 0, len = users_over_30.length; i < len; i++) {
+    names.push(users_over_30[i].name);
 }
 console.log(names);
-// ["ID", "BJ", "JM", "JI"]
+// ["ID", "JM", "JE"]
