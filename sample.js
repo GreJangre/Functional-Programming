d0db288b2832c73aff12cd0db39a6ce8bc0edd8f
Jangre <dlatjdwn8824@naver.com>
2017-12-17 16:02:30 +0900

    값으로서의 함수 표현

--- sample.js
+++ sample.js
@@ -8,3 +8,9 @@ addMaker(10)(5); // 15
 var add5 = addMaker(5);
 add5(3); // 8
 add5(4); // 9
+
+// 값으로서의 함수
+var v1 = 100;
+var v2 = function() {};
+function f1() { return 100; }
+function f2() { return function() {}; }
