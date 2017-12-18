9339135fe2833954435e66fd523cff12e33e3536
Jangre <dlatjdwn8824@naver.com>
2017-12-18 21:11:21 +0900

    ES6 문법으로 표현

--- sample.js
+++ sample.js
@@ -59,22 +59,18 @@ var temp_users = [];
 for (var i = 0; i < users.length; i++) {
     if (users[i].age < 30) { temp_users.push(users[i]); }
 }
-//console.log(temp_users.length);
 // 4
 
 var users_under_30 = filter(users, function(user) { return user.age < 30 });
-//console.log(users_under_30.length);
 // 4
 
 var ages = [];
 for (var i = 0; i < users_under_30.length; i++) {
     ages.push(users_under_30[i].age);
 }
-//console.log(ages);
 // [25, 28, 27, 24]
 
 var ages = map(users_under_30, function(user) { return user.age; });
-//console.log(ages);
 // [25, 28, 27, 24]
 
 // 함수 중첩
@@ -91,22 +87,18 @@ var temp_users = [];
 for (var i = 0; i < users.length; i++) {
     if (users[i].age >= 30) { temp_users.push(users[i]); }
 }
-//console.log(temp_users.length);
 // 3
 
 var users_over_30 = filter(users, function(user) { return user.age >= 30 });
-//console.log(users_over_30.length);
 // 3
 
 var names = [];
 for (var i = 0; i < users_over_30.length; i++) {
     names.push(users_over_30[i].name);
 }
-//console.log(names);
 // ["ID", "JM", "JE"]
 
 var names = map(users_over_30, function(user) { return user.name});
-//console.log(names);
 // ["ID", "JM", "JE"]
 
 // 함수 중첩
@@ -116,3 +108,11 @@ console.log(log_length(
     bvalue('name'))));
 // 3
 // ["ID", "JM", "JE"]
+
+// ES6
+console.log(log_length(
+    map(filter(users, u => u.age < 30), user => user.age)));
+
+console.log(log_length(
+    map(filter(users, u => u.age >= 30), user => user.name)));
+
